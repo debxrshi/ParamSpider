071d76f52cf4b53b78cfdf53cba060afa645cfab
debxrshi <dasdebarshi19@gmail.com>
2024-06-24 22:08:13 +0530

    Cleaned url before saving to file

--- paramspider/client.py
+++ paramspider/client.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from urllib.parse import urlparse, parse_qs
 import random
 import logging
 import time
@@ -10,7 +11,7 @@ import sys
 logging.basicConfig(level=logging.INFO)
 
 
-MAX_RETRIES = 5
+MAX_RETRIES = 2
 
 def write_failed_to_file(url):
     if not os.path.exists("results"):
@@ -19,7 +20,8 @@ def write_failed_to_file(url):
     failed_urls = os.path.join("results","failed_urls.txt")
     
     with open(failed_urls,"w") as f:
-        f.write(url)
+        clean_url = parse_qs(urlparse(url).query).get('url', [None])[0]
+        f.write(clean_url)
 
 
 def load_user_agents():
@@ -45,7 +47,7 @@ def load_user_agents():
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36"
   ]
 
-def fetch_url_content(url,proxy,timeout=5):
+def fetch_url_content(url,proxy,timeout=3):
     """
     Fetches the content of a URL using a random user agent.
     Retries up to MAX_RETRIES times if the request fails.
@@ -67,12 +69,12 @@ def fetch_url_content(url,proxy,timeout=5):
             response.raise_for_status()
             return response
         except (requests.exceptions.RequestException, ValueError):
-            logging.warning(f"Error fetching URL {url}. Retrying in 5 seconds...")
+            logging.warning(f"Error fetching URL {url}. Retrying in {timeout} seconds...")
             time.sleep(timeout)
         except KeyboardInterrupt:
             logging.warning("Keyboard Interrupt received. Exiting gracefully...")
             sys.exit()
 
-    logging.error(f"Failed to fetch URL {url} after {MAX_RETRIES} retries. Moving on to next URL")
+    logging.error(f"Failed to fetch URLs {url} after {MAX_RETRIES} retries. Moving on to next domain")
     write_failed_to_file(url)
     return None
