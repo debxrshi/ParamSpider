68ea71a435a34a98ec5b33d214794019f57eb978
debxrshi <dasdebarshi19@gmail.com>
2024-06-24 20:54:17 +0530

    variable renaming

--- paramspider/client.py
+++ paramspider/client.py
@@ -35,7 +35,7 @@ def load_user_agents():
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36"
   ]
 
-def fetch_url_content(url,proxy,sleep=5):
+def fetch_url_content(url,proxy,timeout=5):
     """
     Fetches the content of a URL using a random user agent.
     Retries up to MAX_RETRIES times if the request fails.
@@ -58,7 +58,7 @@ def fetch_url_content(url,proxy,sleep=5):
             return response
         except (requests.exceptions.RequestException, ValueError):
             logging.warning(f"Error fetching URL {url}. Retrying in 5 seconds...")
-            time.sleep(sleep)
+            time.sleep(timeout)
         except KeyboardInterrupt:
             logging.warning("Keyboard Interrupt received. Exiting gracefully...")
             sys.exit()
